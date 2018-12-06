6708bfc79e5e074d32449a4c7a6a79d2a657098d
Justin Tom <justinttom@gmail.com>
2018-12-06 15:22:42 -0800

    Completed part 1

--- day-03/day3.py
+++ day-03/day3.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Day 3 challenge of the Advent of Code 2018")
 parser.add_argument("-p", "--part", type=int, choices=[1, 2], help="Determines which part of the challenge to run")
@@ -6,11 +7,48 @@ parser.add_argument("-p", "--part", type=int, choices=[1, 2], help="Determines w
 
 def parse_input_file():
 	input_values = open("input.txt", "r")
-	return [box_id.strip() for box_id in input_values]
+	return [specs.strip() for specs in input_values]
+
+def parse_specifications(spec_data):
+	# Sample Spec Data - '#1 @ 100,366: 24x27'
+	spec_sections = spec_data.split(" ")
+	spec = {
+		"number": spec_sections[0][1:],
+		"x_start": int(spec_sections[2].split(",")[0]),
+		"y_start": int(spec_sections[2].split(",")[1][:-1]),
+		"width": int(spec_sections[3].split("x")[0]),
+		"height": int(spec_sections[3].split("x")[1])
+	}
+
+	return spec
+
+def update_fabric_grid(fabric, spec):
+	start_height = spec["y_start"]
+	start_width = spec["x_start"]
+
+	for height_position in range(spec["height"]):
+		for width_position in range(spec["width"]):
+			target_height = start_height + height_position
+			target_width = start_width + width_position
+
+			# Set the value to 'X' if there is an existing value there - else set the value.
+			fabric[target_height, target_width] = "X" if fabric[target_height, target_width] != "" else spec["number"]
+
 
 
 def part_one():
-	pass
+	fabric_height = 1000
+	fabric_width = 1000
+	fabric = np.chararray([fabric_height, fabric_width], itemsize=4, unicode=True)
+	# fabric = [["." for x in range (fabric_width)] for y in range(fabric_height)]
+
+	claim_list = parse_input_file()
+	specs = [parse_specifications(spec) for spec in claim_list]
+
+	for spec in specs:
+		update_fabric_grid(fabric, spec)
+
+	return "The total number of square inches of fabric that are within two or more claims is: " + str(fabric.count("X").sum())
 
 
 def part_two():
